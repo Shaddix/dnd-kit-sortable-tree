dfd1c9d175b5849f7470ea0cd0b049e10b93b430
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2021-10-04 01:26:46 +0700

    export flattenTree

--- dnd-kit-sortable-tree/src/index.ts
+++ dnd-kit-sortable-tree/src/index.ts
@@ -1,7 +1,13 @@
 import { SimpleTreeItemWrapper } from './ui/simple/SimpleTreeItemWrapper';
 import { FolderTreeItemWrapper } from './ui/folder/FolderTreeItemWrapper';
 import { SortableTree, SortableTreeProps } from './SortableTree';
+import { flattenTree } from './utilities';
 import type { TreeItems, TreeItemComponentProps } from './types';
 
-export { SortableTree, SimpleTreeItemWrapper, FolderTreeItemWrapper };
+export {
+  flattenTree,
+  SortableTree,
+  SimpleTreeItemWrapper,
+  FolderTreeItemWrapper,
+};
 export type { TreeItemComponentProps, TreeItems, SortableTreeProps };
